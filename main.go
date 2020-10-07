27bcf2252d8a325659420916ffb84a5cae4b834c
Lars Norlander <lars.nrlndr@gmail.com>
2020-10-07 23:45:19 +0200

    Implement compute gradient

--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 func main() {
-	fmt.Println(blend(rgb(26, 31, 39), rgb(255, 77, 101)))
-	step := steps(26, 255, 12)
-	fmt.Println(step)
-	val := 26.0
-	for i := 0; i < 13; i++ {
-		fmt.Printf("step: %02d\t%f\t%f\n", i, val, math.Round(val))
-		val += step
+	grad := gradient(hex("#1A1F27"), rgb(255, 77, 101), 13)
+	for i := range grad {
+		fmt.Print(grad[i])
 	}
 }
 
@@ -30,6 +27,29 @@ func rgb(r uint8, g uint8, b uint8) Color {
 	}
 }
 
+func hex(s string) Color {
+	var err error
+	color := Color{}
+	s = strings.ToUpper(s)
+
+	switch len(s) {
+	case 7:
+		_, err = fmt.Sscanf(s, "#%02X%02X%02X", &color.R, &color.G, &color.B)
+	case 4:
+		_, err = fmt.Sscanf(s, "#%1X%1X%1X", &color.R, &color.G, &color.B)
+		color.R *= 17
+		color.G *= 17
+		color.B *= 17
+	default:
+		err = fmt.Errorf("invalid hex")
+	}
+
+	if err != nil {
+		panic(err)
+	}
+	return color
+}
+
 func (c Color) String() string {
 	return c.AsHex()
 }
@@ -39,7 +59,7 @@ func (c Color) AsRGB() string {
 }
 
 func (c Color) AsHex() string {
-	return fmt.Sprintf("#%x%x%x", c.R, c.G, c.B)
+	return fmt.Sprintf("#%X%X%X", c.R, c.G, c.B)
 }
 
 func blend(x Color, y Color) Color {
@@ -62,9 +82,28 @@ func steps(x uint8, y uint8, steps uint) float64 {
 	return float64(diff) / float64(steps)
 }
 
-func gradient(x Color, y Color) [15]Color {
-	grad := [15]Color{}
-	grad[0] = x
-	grad[14] = y
+func gradient(x Color, y Color, between uint) []Color {
+	grad := make([]Color, 2+between)
+	stepCount := between + 1
+
+	rStep := steps(x.R, y.R, stepCount)
+	gStep := steps(x.G, y.G, stepCount)
+	bStep := steps(x.B, y.B, stepCount)
+
+	rCur := float64(x.R)
+	gCur := float64(x.G)
+	bCur := float64(x.B)
+
+	for i := 0; i < len(grad); i++ {
+		grad[i] = rgb(
+			uint8(math.Round(rCur)),
+			uint8(math.Round(gCur)),
+			uint8(math.Round(bCur)),
+		)
+		rCur += rStep
+		gCur += gStep
+		bCur += bStep
+	}
+
 	return grad
 }
